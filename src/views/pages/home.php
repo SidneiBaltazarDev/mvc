94b74334463ff61bdb87c7b617fbd98715696d7b
Sidnei Baltazar Segatto Junior <157621521+SidneiBaltazarDev@users.noreply.github.com>
2024-09-11 10:55:08 -0300

    Create and Read
    Funcionalidade de create, read implementadas utilizando a arquitetura MVC.

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -4,5 +4,31 @@
 
 <a href="<?=$base;?>/novo">Novo Usuário</a>
 
+<table border="1" width="100%">
+    <tr>
+        <td>ID</td>
+        <td>NOME</td>
+        <td>E-MAIL</td>
+        <td>EDITAR</td>
+        <td>EXCLUIR</td>
+    </tr>
+    <?php foreach($usuarios as $usuario): ?>
+
+        <tr>
+            <td><?=$usuario['id'];?></td>
+            <td><?=$usuario['nome'];?></td>
+            <td><?=$usuario['email'];?></td>
+            <td>
+                <a href="<?=$base;?>/usuario/<?=$usuario['id'];?>/editar">Editar</a>
+            </td>
+            <td>
+                <a href="<?=$base;?>/usuario/<?=$usuario['id'];?>/excluir">Excluir</a>
+            </td>
+            
+        </tr>
+
+    <?php endforeach?>
+</table>
+
 
 <?php $render('Footer'); ?>
